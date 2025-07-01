f2630da1b05c3c5c5bc447e25f848c67e5fc3de6
vaish2662 <vaishnavipadwal1999@gmail.com>
2025-07-01 12:27:06 +0530

    usercard

--- react_frontend_task/src/components/UserCard.jsx
+++ react_frontend_task/src/components/UserCard.jsx
@@ -4,6 +4,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col'
 import Badge from 'react-bootstrap/Badge';
 import Table from 'react-bootstrap/Table';
+import Card from 'react-bootstrap/Card';
+
 
 function UserCard({name,email,role}) {
      const [userCard]=useState([{name:"Sejal Patil",email:"sejal@gmail.com",role:"Frontend Developer"},{name:"Ram Deshmukh", email:"ram@gmail.com",role:"Backend Developer"},{name:"Pranjal Chavhan", email:"pranjal@gmail.com", role:"Full stack developer"}])
@@ -41,6 +43,25 @@ function UserCard({name,email,role}) {
           
      </tr>
         ))};
+
+               
+        <div>
+            {userCard.map((item,index)=>(
+            // <li key={index}>{item.name}</li>
+            <Card className='bg-gray' style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{item.name}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted"><Badge bg="primary">{item.role}</Badge></Card.Subtitle>
+        <Card.Text>
+          {item.email}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+        ))}
+        </div>
+
+        
+
      </tbody>
       </Table>
         
