b3f4d0beebc6950941e38dd0668b433fd02092a9
vaish2662 <vaishnavipadwal1999@gmail.com>
2025-07-01 11:42:28 +0530

    second

--- node.js/index.js
+++ node.js/index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
 app.get('/api/hello',(req,res)=>{
     res.send("<h2>hello World!</h2>")
+     res.status(200).send({
+            success:true,
+            message:"Hello world",
+            
+        })
     
 })
 
